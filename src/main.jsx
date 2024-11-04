66a5ab5c296605b8c925970040e562f2a552b124
Subroto Biswas <37511552+toofaniCoder@users.noreply.github.com>
2024-11-04 23:15:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 // import Roboto Font
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -10,17 +12,25 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { CssBaseline, GlobalStyles } from "@mui/material";
 
+const theme = createTheme({
+ // your theme goes here...
+});
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CssBaseline />
-    <GlobalStyles
-      styles={{
-        body: {
-          backgroundImage: "radial-gradient(rgba(0,0,0,0.5) 1px, transparent 0)",
-          backgroundSize:"25px 25px"
-        },
-      }}
-    />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          body: {
+            backgroundImage:
+              "radial-gradient(rgba(0,0,0,0.5) 1px, transparent 0)",
+            backgroundSize: "25px 25px",
+          },
+        }}
+      />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
